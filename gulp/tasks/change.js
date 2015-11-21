0f7cfa10f5dbc9344bb7ffa9adb436ecac24502c
Masa <masa@bespoke.is>
2015-11-21 15:25:46 -0800

    adding change name method

--- gulp/tasks/change.js
+++ gulp/tasks/change.js
@@ -8,27 +8,40 @@ var gulp = require('gulp'),
     chalk = require('chalk');
 
 module.exports = gulp.task('change:name', function () {
-  var all_app_path = [
-    APP_PATH + "/gulp/**/*.js",
-    APP_PATH + "/gulp/*.js",
-    APP_PATH + "/src/*",
-    APP_PATH + "/src/**/*",
-    APP_PATH + "/e2e/**/*",
-    APP_PATH + "/e2e/*",
-    APP_PATH + "/config/**/*",
-    APP_PATH + "/config/*",
-    APP_PATH + "/*",
-  ];
-  return gulp.src(all_app_path)
+  var all_path = {
+    "gulp" : [
+      APP_PATH + "/gulp/**/*.js",
+      APP_PATH + "/gulp/*.js"
+    ],
+    "src" : [ 
+      APP_PATH + "/src/*",
+      APP_PATH + "/src/**/*"
+    ],
+    "e2e" : [
+      APP_PATH + "/e2e/**/*",
+      APP_PATH + "/e2e/*"
+    ],
+    "config" : [
+      APP_PATH + "/config/**/*",
+      APP_PATH + "/config/*"
+    ],
+    "": APP_PATH + "/*"  
+  };
+  
+  gulp.src(APP_PATH)
     .pipe(prompt.prompt([{
         type: 'input',
         name: 'app_name',
         message: "What is the name of this app?",
         default: "app"
     }], function(res){
-      gulp.src(all_app_path)
-        .pipe(replace("ionic-base", res.app_name))
-        .pipe(gulp.dest(APP_PATH));
+      
+      for(var key in all_path){
+        gulp.src(all_path[key])
+          .pipe(replace("neighborchef", res.app_name))
+          .pipe(gulp.dest(APP_PATH + "/" + key));
+      }
+      
     }));
   
 });
